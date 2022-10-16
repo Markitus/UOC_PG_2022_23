92f7c48369233af674fb174328ea28d21deb5d68
eteg09 <eterarroyo@gmail.com>
2022-10-16 15:46:48 +0200

    Add files via upload

--- PAC2/Lessons/07_ Sprite_animation.c.c
+++ PAC2/Lessons/07_ Sprite_animation.c.c
@@ -32,7 +32,7 @@ int main(void)
     Texture2D scarfy = LoadTexture("resources/scarfy.png");        // Texture loading
 
     Vector2 position = { 350.0f, 280.0f };
-    Rectangle frameRec = { 0.0f, 0.0f, (float)scarfy.width/6, (float)scarfy.height };
+    Rectangle frameRec = { 0.0f, 0.0f, (float)scarfy.width / 6, (float)scarfy.height };
     int currentFrame = 0;
 
     int framesCounter = 0;
@@ -48,14 +48,14 @@ int main(void)
         //----------------------------------------------------------------------------------
         framesCounter++;
 
-        if (framesCounter >= (60/framesSpeed))
+        if (framesCounter >= (60 / framesSpeed))
         {
             framesCounter = 0;
             currentFrame++;
 
             if (currentFrame > 5) currentFrame = 0;
 
-            frameRec.x = (float)currentFrame*(float)scarfy.width/6;
+            frameRec.x = (float)currentFrame * (float)scarfy.width / 6;
         }
 
         // Control frames speed
@@ -70,25 +70,25 @@ int main(void)
         //----------------------------------------------------------------------------------
         BeginDrawing();
 
-            ClearBackground(RAYWHITE);
+        ClearBackground(RAYWHITE);
 
-            DrawTexture(scarfy, 15, 40, WHITE);
-            DrawRectangleLines(15, 40, scarfy.width, scarfy.height, LIME);
-            DrawRectangleLines(15 + (int)frameRec.x, 40 + (int)frameRec.y, (int)frameRec.width, (int)frameRec.height, RED);
+        DrawTexture(scarfy, 15, 40, WHITE);
+        DrawRectangleLines(15, 40, scarfy.width, scarfy.height, LIME);
+        DrawRectangleLines(15 + (int)frameRec.x, 40 + (int)frameRec.y, (int)frameRec.width, (int)frameRec.height, RED);
 
-            DrawText("FRAME SPEED: ", 165, 210, 10, DARKGRAY);
-            DrawText(TextFormat("%02i FPS", framesSpeed), 575, 210, 10, DARKGRAY);
-            DrawText("PRESS RIGHT/LEFT KEYS to CHANGE SPEED!", 290, 240, 10, DARKGRAY);
+        DrawText("FRAME SPEED: ", 165, 210, 10, DARKGRAY);
+        DrawText(TextFormat("%02i FPS", framesSpeed), 575, 210, 10, DARKGRAY);
+        DrawText("PRESS RIGHT/LEFT KEYS to CHANGE SPEED!", 290, 240, 10, DARKGRAY);
 
-            for (int i = 0; i < MAX_FRAME_SPEED; i++)
-            {
-                if (i < framesSpeed) DrawRectangle(250 + 21*i, 205, 20, 20, RED);
-                DrawRectangleLines(250 + 21*i, 205, 20, 20, MAROON);
-            }
+        for (int i = 0; i < MAX_FRAME_SPEED; i++)
+        {
+            if (i < framesSpeed) DrawRectangle(250 + 21 * i, 205, 20, 20, RED);
+            DrawRectangleLines(250 + 21 * i, 205, 20, 20, MAROON);
+        }
 
-            DrawTextureRec(scarfy, frameRec, position, WHITE);  // Draw part of the texture
+        DrawTextureRec(scarfy, frameRec, position, WHITE);  // Draw part of the texture
 
-            DrawText("(c) Scarfy sprite by Eiden Marsal", screenWidth - 200, screenHeight - 20, 10, GRAY);
+        DrawText("(c) Scarfy sprite by Eiden Marsal", screenWidth - 200, screenHeight - 20, 10, GRAY);
 
         EndDrawing();
         //----------------------------------------------------------------------------------
