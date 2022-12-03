930caf6a423e8c7ada62a2fe437d8feef78ec506
eteg09 <eterarroyo@gmail.com>
2022-12-03 19:06:40 +0000

    Add files via upload

---
+++ PAC3/Lessons/05_maze_creation_camera3D_FirstPerson.c
@@ -0,0 +1,95 @@
+/*******************************************************************************************
+*
+*   Challenge 03:   MAZE GAME
+*   Lesson 05:      Camera3D Projection
+*   Description:    Camera 3D creation and management (camera first person)
+*
+*   Copyright (c) 2017-2022 Ramon Santamaria (@raysan5)
+*
+********************************************************************************************/
+
+#include "raylib.h"
+
+#include <stdlib.h>           // Required for: free()
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 1020;
+    const int screenHeight = 800;
+
+    InitWindow(screenWidth, screenHeight, "CHALLENGE 03: FIRST PERSON MAZE");
+
+    // LESSON 05: Define the camera to look into our 3d world (Next chapter will be explain more thoroughly)
+    Camera3D camera = { 0 };
+    camera.position = (Vector3){ 4.0f, 0.5f, 4.0f };  // Camera position
+    camera.target = (Vector3){ 0.0f, 1.8f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 60.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_PERSPECTIVE;             // Camera mode type
+
+
+    // LESSON05: We load the texture used by the Cubicmap
+    Image imMap = LoadImage("resources/cubicmap.png");      // Load cubicmap image (RAM)
+    Texture2D cubicmap = LoadTextureFromImage(imMap);       // Convert image to texture to display (VRAM)
+    UnloadImage(imMap);             // Unload image from RAM
+
+    Mesh mesh = GenMeshCubicmap(imMap, (Vector3) { 1.0f, 1.0f, 1.0f });
+    Model model = LoadModelFromMesh(mesh);
+
+    // NOTE: By default each cube is mapped to one part of texture atlas
+    Texture2D texture = LoadTexture("resources/cubemap_atlas.png");    // Load map texture
+    model.materials[0].maps[MATERIAL_MAP_DIFFUSE].texture = texture;             // Set map diffuse texture
+
+    Vector3 mapPosition = { 0.0f, 0.0f, 0.0f };  // Set model position
+
+    // LESSON05: Set a first camera person mode
+    SetCameraMode(camera, CAMERA_FIRST_PERSON);
+
+    SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        UpdateCamera(&camera);
+
+       //----------------------------------------------------------------------------------
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        BeginMode3D(camera);
+            DrawModel(model, mapPosition, 1.0f, WHITE);                     // Draw maze map
+        EndMode3D();
+
+        // LESSON05: Instructions to move the camera3D.
+        DrawText("First person camera default controls:", 20, 20, 10, WHITE);
+        DrawText("- Move with keys: W, A, S, D", 40, 40, 10, WHITE);
+        DrawText("- Mouse move to look around", 40, 60, 10, WHITE);
+
+        EndDrawing();
+        //----------------------------------------------------------------------------------
+    }
+
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    UnloadImageColors(mapPixels);   // Unload color array
+
+    UnloadTexture(cubicmap);        // Unload cubicmap texture
+    UnloadTexture(texture);         // Unload map texture
+
+    CloseWindow();                  // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
